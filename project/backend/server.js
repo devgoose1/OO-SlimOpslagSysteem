de63f1fa4dabb99df70cbcc004b9b7b116f6a101
natieboy <natieboysupercool@gmail.com>
2025-12-16 13:19:56 +0100

    Eerste kleine testcode van server.js. Ook begonnen aan de database maken met database.js. daarnaast nog extra structuur gemaakt voor de backend

---
+++ project/backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hallo wereld!');
+});
+
+app.listen(port, () => {
+    console.log(`Server staat aan op http://localhost:${port}`);
+});
