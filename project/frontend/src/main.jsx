ef7ac465f715194b6b11f3d57e9ff8b26f331ee0
Nathan Schinkel <139856176+devgoose1@users.noreply.github.com>
2025-12-29 13:31:07 +0100

    Wrap RootApp with HashRouter for routing

--- project/frontend/src/main.jsx
+++ project/frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // <<< toegevoegd
 import './index.css'
 import App from './App.jsx'
 import { registerServiceWorker } from './services/pwaService.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RootApp />
+    <HashRouter> {/* <<< toegevoegd */}
+      <RootApp />
+    </HashRouter>
   </StrictMode>,
 )
 
